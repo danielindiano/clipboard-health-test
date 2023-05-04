8686282a1beca3a5e82256924afbeb32d735c2a6
Daniel Indiano <daniel.indiano@gmail.com>
2023-05-03 21:21:02 -0300

    test: Including new test cases

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,35 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("Returns the event partition key when it is set", () => {
+    const event = {
+      data: "Anything",
+      partitionKey: "the-partition-key",
+    };
+
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe("the-partition-key");
+  });
+
+  it("Returns the hashed event when there's no partitionKey in event", () => {
+    const event = {
+      data: "Anything",
+      notAPartitionKey: "the-partition-key",
+    };
+
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).not.toBe("the-partition-key");
+  });
+
+  it("Returns a hashed version of Partition Key when its length exceeds the max_partition_key_length", () => {
+    const event = {
+      data: "Anything",
+      partitionKey:
+        "this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-",
+    };
+
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).not.toBe(event.partitionKey);
+  });
 });
--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-const {deterministicPartitionKey} = require("./dpk");
+const { deterministicPartitionKey } = require("./dpk");
 
-console.log(deterministicPartitionKey());
+// No candidate
+{
+  const trivialKey = deterministicPartitionKey();
+  console.log(trivialKey);
+}
+
+// With Partition Key
+{
+  const event = {
+    data: "Anything",
+    partitionKey: "the-partition-key",
+  };
+
+  const trivialKey = deterministicPartitionKey(event);
+  console.log(trivialKey);
+}
+
+// Without Partition Key
+{
+  const event = {
+    data: "Anything",
+    notAPartitionKey: "not-the-partition-key",
+  };
+
+  const trivialKey = deterministicPartitionKey(event);
+  console.log(trivialKey);
+}
+
+// With bigger Partition Key (> MAX_PARTITION_KEY_LENGTH)
+{
+  const event = {
+    data: "Anything",
+    partitionKey:
+      "this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-this-is-a-big-partition-key-",
+  };
+
+  const trivialKey = deterministicPartitionKey(event);
+  console.log(trivialKey);
+}
