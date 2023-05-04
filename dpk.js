bb787c7b5567f3974e36de244619692f4563fac7
Daniel Indiano <daniel.indiano@gmail.com>
2023-05-03 22:26:36 -0300

    refact: Including a error handling

--- dpk.js
+++ dpk.js
@@ -9,19 +9,27 @@ exports.deterministicPartitionKey = (event) => {
   // If there'no event or it's falsy, then return the trivial key
   if (!event) return TRIVIAL_PARTITION_KEY;
 
-  // If there's no partition key on event, then return the whole event encrypted
-  if (!event.partitionKey) {
-    return encryptData(JSON.stringify(event));
+  try {
+    // If there's no partition key on event, then return the whole event encrypted
+    if (!event.partitionKey) {
+      return encryptData(JSON.stringify(event));
+    }
+
+    // When event.partitionKey is not a string, we stringify it
+    const partitionKeyCandidate =
+      typeof event.partitionKey === "string"
+        ? event.partitionKey
+        : JSON.stringify(event.partitionKey);
+
+    // Returns the candidate or it's encrypted hash when its length exceeds the max_partition_key_length
+    return partitionKeyCandidate.length <= MAX_PARTITION_KEY_LENGTH
+      ? partitionKeyCandidate
+      : encryptData(partitionKeyCandidate);
+  } catch (error) {
+    console.log(
+      "An error ocurred while generating partitionKey. Error: ",
+      error
+    );
+    return TRIVIAL_PARTITION_KEY;
   }
-
-  // When event.partitionKey is not a string, we stringify it
-  const candidate =
-    typeof event.partitionKey === "string"
-      ? event.partitionKey
-      : JSON.stringify(event.partitionKey);
-
-  // Returns the candidate or it's encrypted hash when its length exceeds the max_partition_key_length
-  return candidate.length <= MAX_PARTITION_KEY_LENGTH
-    ? candidate
-    : encryptData(candidate);
 };
